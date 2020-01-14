0dc16f30826835dcf056fc3bf06f48b35157ce43
Brysond <bryson.d@etoncollege.org.uk>
2020-01-14 10:29:27 +0000

    Birth of the Space-being

--- CarCustomizer/CarCustomizer/StarterCars.swift
+++ CarCustomizer/CarCustomizer/StarterCars.swift
@@ -9,5 +9,5 @@
 import Foundation
 
 struct StarterCars {
-    var cars = [Car(make: "Mazda", model: "MX-5", topSpeed: 125, acceleration: 7.7, handling: 5), Car(make: "Toyota", model: "Prius", topSpeed: 100, acceleration: 8.5, handling: 10)]
+    var cars = [Car(make: "Mazda", model: "MX-5", topSpeed: 230, acceleration: 7.7, handling: 5), Car(make: "Toyota", model: "Prius", topSpeed: 180, acceleration: 8.5, handling: 10), Car(make: "Reliant", model: "Robin", topSpeed: 128, acceleration: 60, handling: 0)]
 }
---
+++ Tamagotchi_App/Tamagotchi_App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tamagotchi_App
+//
+//  Created by Bryson, David (IRG) on 14/01/2020.
+//  Copyright © 2020 David Bryson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
