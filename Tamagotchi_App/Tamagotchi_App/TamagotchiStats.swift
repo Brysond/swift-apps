a6a1bda867bbe745fee6fcfa2a6dbd2ada39dbc2
Brysond <bryson.d@etoncollege.org.uk>
2020-01-14 10:42:31 +0000

    V2

---
+++ Tamagotchi_App/Tamagotchi_App/TamagotchiStats.swift
@@ -0,0 +1,43 @@
+//
+//  TamagotchiStats.swift
+//  Tamagotchi_App
+//
+//  Created by Bryson, David (IRG) on 14/01/2020.
+//  Copyright © 2020 David Bryson. All rights reserved.
+//
+
+import Foundation
+
+struct TamagotchiStats {
+    private var age = 0
+    private var happy = 0
+    private var hunger = 0
+    private var weight = 5
+    private var discipline = 0
+    
+    func getAge() -> Int {
+        return age
+    }
+    func getHappiness() -> Int {
+        return happy
+    }
+    func getHunger() -> Int {
+        return hunger
+    }
+    func getWeight() -> Int {
+        return weight
+    }
+    func getDiscipline() -> Int {
+        return discipline
+    }
+    
+    func displayStats() -> String {
+        return """
+            Age: \(age)
+            Hapy: \(happy)
+            Hunger: \(hunger)
+            Mass: \(weight)
+            
+            """
+    }
+}
