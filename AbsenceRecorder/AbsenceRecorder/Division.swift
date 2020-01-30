68ab70138d4775124e4060199ee58f9a5a4e0c9e
Brysond <bryson.d@etoncollege.org.uk>
2020-01-30 08:57:21 +0000

    First

---
+++ AbsenceRecorder/AbsenceRecorder/Division.swift
@@ -0,0 +1,18 @@
+//
+//  Division.swift
+//  AbsenceRecorder
+//
+//  Created by Bryson, David (IRG) on 30/01/2020.
+//  Copyright © 2020 David Bryson. All rights reserved.
+//
+
+import Foundation
+
+class Division {
+    let code: String
+    var students: [Student] = []
+    
+    init(code: String) {
+        self.code = code
+    }
+}
